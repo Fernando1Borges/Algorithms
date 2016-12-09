fcfcd1d9b4b2ccbfbd12a34ea89d9672b580cc2f
Fernando1Borges <centraloro@gmail.com>
2016-12-09 11:31:53 -0600

    Add official solutions to pairwise

--- pairwise.js
+++ pairwise.js
@@ -23,3 +23,55 @@ function pairwise(arr, arg) {
 // pairwise([1, 1, 1], 2) should return 1.
 // pairwise([0, 0, 0, 0, 1, 1], 1) should return 10.
 // pairwise([], 100) should return 0.
+
+
+
+// Official Advanced Code Solution
+
+function pairwise(arr, arg) {
+  // search array for elements that when paired, equal the second argument, then sum their indices
+  // make a local copy of the arguments object so we don't modify it directly
+  var pairArr = arr.slice();
+  return pairArr.reduce( function (a,b,index){ // use native reduce to collect running total of summed indices
+      var search = arg - b; // get difference of current item so we know what value will sum to arg
+
+      // check if search value in rest of the array, but also make sure it doesn't match current search index
+      if ( pairArr.indexOf(search) != -1 && pairArr.indexOf(search) != index ){
+         var total = index + pairArr.indexOf(search);  // if found, add to the runnning total
+         pairArr.splice(index,1,NaN); // remove current index from the array
+         pairArr.splice(pairArr.indexOf(search),1,NaN); // remove the other matched element from the array
+         return a + total; //return the running total back to reduce for next item
+      }
+      return a; // simply return previous total if no operations needed
+  },0);
+
+}
+
+// test here
+pairwise([1,4,2,3,0,5], 7);
+
+// Official Basic Code Solution
+
+function pairwise(arr, arg) {
+ // Set sum of indices to zero
+ var sum = 0;
+ // make a local copy of the arguments object so we don't modify it directly
+ var pairArr = arr.slice();
+ // looping from first element
+ for(i = 0; i < pairArr.length; i++) {
+   //Looping from second element by setting first element  constant
+   for(j = i + 1; j < pairArr.length; j++) {
+     // Check whether the sum is equal to arg
+     if(pairArr[i] + pairArr[j] == arg) {
+       //Add the indices
+       sum += i + j;
+       //Set the indices to NaN so that they can't be used in next iteration
+       pairArr[i] = pairArr[j] = NaN;
+     }
+   }
+ }
+ return sum;
+}
+
+// test here
+pairwise([1,4,2,3,0,5], 7);
